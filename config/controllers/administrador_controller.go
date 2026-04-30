8daabb91237831dcbb1d4fe2085bfe0862ec36d7
Soy Alejandro <pilodaniel4000@gmail.comgit config --listgit config --global user.email pilodaniel4000@gmail.com>
2026-04-29 19:01:59 -0500

    se agraga func 3

--- config/controllers/administrador_controller.go
+++ config/controllers/administrador_controller.go
@@ -48,3 +48,32 @@ func GetAllAdministradores(w http.ResponseWriter, r *http.Request) {
 	}
 	respondJSON(w, 200, admins)
 }
+
+func GetAdministradorByID(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	var a models.Administrador
+
+	err := config.DB.QueryRow(
+		`SELECT id_administrador, nombre_gym, nit, anio_fundacion, cantidad_clientes,
+		departamento, ciudad, direccion, propietario_nombre, telefono, correo,
+		pagina_web, firma, fecha_firma, estado, activo, fecha_modificacion, fecha_creacion
+		FROM administrador WHERE id_administrador=$1`,
+		id,
+	).Scan(
+		&a.IDAdministrador, &a.NombreGym, &a.Nit, &a.AnioFundacion,
+		&a.CantidadClientes, &a.Departamento, &a.Ciudad, &a.Direccion,
+		&a.PropietarioNombre, &a.Telefono, &a.Correo, &a.PaginaWeb,
+		&a.Firma, &a.FechaFirma, &a.Estado, &a.Activo,
+		&a.FechaModificacion, &a.FechaCreacion,
+	)
+
+	if err == sql.ErrNoRows {
+		respondJSON(w, 404, map[string]string{"error": "Administrador no encontrado"})
+		return
+	}
+	if err != nil {
+		respondJSON(w, 500, map[string]string{"error": err.Error()})
+		return
+	}
+	respondJSON(w, 200, a)
+}
