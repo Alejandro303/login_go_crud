08141a9a22ac4fa0fc8aa841a79a3825b9f31422
Soy Alejandro <pilodaniel4000@gmail.comgit config --listgit config --global user.email pilodaniel4000@gmail.com>
2026-04-29 19:58:43 -0500

    se agrega func 20

---
+++ LOGIN_GO_CRUD/routes/usuario_routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"API_THEHOUSEFIT/controllers"
+	"github.com/gorilla/mux"
+)
+
+func RegisterUsuarioRoutes(r *mux.Router) {
+	r.HandleFunc("/usuarios", controllers.GetAllUsuarios).Methods("GET")
+	r.HandleFunc("/usuarios/{id}", controllers.GetUsuarioByID).Methods("GET")
+	r.HandleFunc("/usuarios", controllers.CreateUsuario).Methods("POST")
+	r.HandleFunc("/usuarios/{id}", controllers.UpdateUsuario).Methods("PUT")
+	r.HandleFunc("/usuarios/{id}", controllers.DeleteUsuario).Methods("DELETE")
+}
